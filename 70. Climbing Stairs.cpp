0eed4bb04735371d6fd53a1652531b9982468112
Aman Kumar <100454472+amankr1469@users.noreply.github.com>
2024-01-18 19:02:27 +0530

    18-Jan

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+private:
+    int solve(int n, vector<int>& dp){
+        if(n<=2) return n;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
+    }
+};
